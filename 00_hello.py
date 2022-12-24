4b88e0bf8131a2304303bf9d385fa229c8f5ed99
PedroConcejo <60395502+PedroConcejo@users.noreply.github.com>
2022-12-24 08:11:29 +0000

    session 0 & 1

---
+++ 00_hello.py
@@ -0,0 +1,17 @@
+# Hola Mundo
+print('Hello World')
+"""
+Cometario
+en
+varias
+lineas
+"""
+# Checking data types
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Asabeneh'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'Pedro'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
---
+++ 01_variables.py
@@ -0,0 +1,22 @@
+# Variables:
+
+
+# Variables in Python
+first_name = 'Pedro'
+last_name = 'Concejo'
+country = 'Spain'
+city = 'Lanzarote'
+age = 33
+is_married = True
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+person_info = {
+   'firstname':first_name,
+   'lastname':last_name,
+   'country':country,
+   'city':city
+   }
+nombre, apellido, alias = 'Pedro', 'Concejo', 'PedroDev'
+
+print(person_info.get('firstname'))
+print(len(city))
+print('👋 Howdy')
